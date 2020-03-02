07fd711cb06f2070858568d8021b02577741718e
dev.bahaasrour@gmail.com <dev.bahaasrour@gmail.com>
2020-03-02 10:23:13 +0200

    firstcommit2

---
+++ Assets/Scripts/PlayerCameraRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraRotation : MonoBehaviour
+{
+    public Transform Myplayer;
+    [SerializeField] Vector3 offset;
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.transform.position = Myplayer.position + offset;
+    }
+}
